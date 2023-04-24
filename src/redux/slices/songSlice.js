e1d2511f3d1cab8872ed2d9856386ad4f2f78f40
Md. Tajminur Rahman <tajminur.aiub@gmail.com>
2023-04-24 10:22:35 +0600

    updated APIs from render server

--- src/redux/slices/songSlice.js
+++ src/redux/slices/songSlice.js
@@ -1,42 +1,44 @@
-import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
-export const fetchAllSongs = createAsyncThunk(
-    'song/fetchSongs',
-    async () => {
-        const response = await fetch('https://murmuring-brook-63531.herokuapp.com/songs').then(res => res.json())
-        return response;
-    }
-)
-
-export const songSlice = createSlice({
-    name: 'song',
-    initialState: {
-        myPlaylist: [],
-        songs: [],
-        isLoading: true
-    },
-    reducers: {
-        addToPlayList: (state, action) => {
-            state.myPlaylist.push(action.payload)
-        },
-        removeFromPlayList: (state, { payload }) => {
-            state.myPlaylist = state.myPlaylist.filter(song => song._id !== payload._id)
-        },
-        clearPlayList: (state, action) => {
-            state.myPlaylist = [];
-        }
-    },
-    extraReducers: (builder) => {
-        builder.addCase(fetchAllSongs.fulfilled, (state, action) => {
-            state.songs = action.payload;
-            state.isLoading = false;
-        })
-        builder.addCase(fetchAllSongs.pending, (state, action) => {
-            state.isLoading = true;
-        })
-    },
+export const fetchAllSongs = createAsyncThunk("song/fetchSongs", async () => {
+  const response = await fetch("https://muzify-server.onrender.com/songs").then(
+    (res) => res.json()
+  );
+  return response;
 });
 
-export const { addToPlayList, removeFromPlayList, clearPlayList } = songSlice.actions;
+export const songSlice = createSlice({
+  name: "song",
+  initialState: {
+    myPlaylist: [],
+    songs: [],
+    isLoading: true,
+  },
+  reducers: {
+    addToPlayList: (state, action) => {
+      state.myPlaylist.push(action.payload);
+    },
+    removeFromPlayList: (state, { payload }) => {
+      state.myPlaylist = state.myPlaylist.filter(
+        (song) => song._id !== payload._id
+      );
+    },
+    clearPlayList: (state, action) => {
+      state.myPlaylist = [];
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(fetchAllSongs.fulfilled, (state, action) => {
+      state.songs = action.payload;
+      state.isLoading = false;
+    });
+    builder.addCase(fetchAllSongs.pending, (state, action) => {
+      state.isLoading = true;
+    });
+  },
+});
+
+export const { addToPlayList, removeFromPlayList, clearPlayList } =
+  songSlice.actions;
 
 export default songSlice.reducer;
